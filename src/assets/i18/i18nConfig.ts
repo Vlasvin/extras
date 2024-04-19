cd39f81b63ea1a6eda1ff2ddc83f98b2457f599c
Oleksii Vlasenko <vlasvin@gmail.com>
2024-04-19 19:07:38 +0300

    added dependencies set i18

---
+++ src/assets/i18/i18nConfig.ts
@@ -0,0 +1,28 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const language = navigator.language.split("-")[0];
+
+i18n.use(initReactI18next).init({
+  resources: {
+    [language]: {
+      translation: {},
+    },
+    uk: {
+      translation: {},
+    },
+    de: {
+      translation: {},
+    },
+    en: {
+      translation: {},
+    },
+  },
+  lng: language,
+  fallbackLng: "uk",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
