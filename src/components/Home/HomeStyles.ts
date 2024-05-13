7f16f7c2a4102c04011f64072a9f741896183db7
Oleksii Vlasenko <vlasvin@gmail.com>
2024-05-13 21:07:44 +0300

    set up home

---
+++ src/components/Home/HomeStyles.ts
@@ -0,0 +1,20 @@
+export const homeStyles = {
+  button: {
+    fontFamily: "Montserrat, sans-serif",
+    backgroundColor: "transparent",
+    fontSize: "smaller",
+    margin: "0px",
+    borderRadius: "10px",
+  },
+  title: {
+    marginBottom: "30px",
+    paddingLeft: "40px",
+    fontSize: "clamp(24px,5.5vw,28px)",
+  },
+  img: {
+    borderRadius: "20px",
+    width: "420px",
+    height: "420px",
+    marginBottom: "48px",
+  },
+};
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,44 +1,99 @@
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, Button } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { homeStyles } from "./HomeStyles";
+import goldenGate from "assets/pictures/jpg/golden-gate-bridge.jpg";
+import canadaToronto from "assets/pictures/jpg/canada_toronto.jpg";
+import australiaSydney from "assets/pictures/jpg/australia-sidney.jpg";
 
 const Home = () => {
   const { t } = useTranslation();
 
   return (
     <div>
-      <Typography variant="h1">{t("home.visa_usa.title")}</Typography>
+      <Typography variant="h1" style={homeStyles.title}>
+        {t("home.visa_usa.title")}
+      </Typography>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <img src="/path/to/image_usa.jpg" alt="USA" />
+        <Grid item xs={12} md={4}>
+          <img src={goldenGate} alt="USA" style={homeStyles.img} />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography variant="body1">
-            {t("home.visa_usa.description")}
-          </Typography>
+        <Grid item xs={12} md={8} style={{ paddingLeft: "32px" }}>
+          <Typography
+            variant="body1"
+            dangerouslySetInnerHTML={{ __html: t("home.visa_usa.description") }}
+          />
+          <Typography
+            variant="body1"
+            dangerouslySetInnerHTML={{ __html: t("home.visa_usa.joke") }}
+          />
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              component={Link}
+              to="/visas/usa"
+              style={homeStyles.button}
+            >
+              {t("home.visa_usa.details")}
+            </Button>
+          </div>
         </Grid>
       </Grid>
 
-      <Typography variant="h1">{t("home.visa_canada.title")}</Typography>
+      <Typography variant="h1" style={homeStyles.title}>
+        {t("home.visa_canada.title")}
+      </Typography>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <img src="/path/to/image_canada.jpg" alt="Canada" />
+        <Grid item xs={12} md={4}>
+          <img src={canadaToronto} alt="Canada" style={homeStyles.img} />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography variant="body1">
-            {t("home.visa_canada.description")}
-          </Typography>
+        <Grid item xs={12} md={8} style={{ paddingLeft: "32px" }}>
+          <Typography
+            variant="body1"
+            dangerouslySetInnerHTML={{
+              __html: t("home.visa_canada.description"),
+            }}
+          ></Typography>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              component={Link}
+              to="/visas/canada"
+              style={homeStyles.button}
+            >
+              {t("home.visa_canada.details")}
+            </Button>
+          </div>
         </Grid>
       </Grid>
 
-      <Typography variant="h1">{t("home.visa_australia.title")}</Typography>
+      <Typography variant="h1" style={homeStyles.title}>
+        {t("home.visa_australia.title")}
+      </Typography>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <img src="/path/to/image_australia.jpg" alt="Australia" />
+        <Grid item xs={12} md={4}>
+          <img src={australiaSydney} alt="Australia" style={homeStyles.img} />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography variant="body1">
-            {t("home.visa_australia.description")}
-          </Typography>
+        <Grid item xs={12} md={8} style={{ paddingLeft: "32px" }}>
+          <Typography
+            variant="body1"
+            dangerouslySetInnerHTML={{
+              __html: t("home.visa_australia.description"),
+            }}
+          />
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              component={Link}
+              to="/visas/australia"
+              style={homeStyles.button}
+            >
+              {t("home.visa_australia.details")}
+            </Button>
+          </div>
         </Grid>
       </Grid>
     </div>
