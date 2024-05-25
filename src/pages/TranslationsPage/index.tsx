c46029edd634807fe3cb42898ed57c6765133cc4
Oleksii Vlasenko <vlasvin@gmail.com>
2024-05-25 17:24:14 +0300

    added photo to aboutUs

--- src/pages/TranslationsPage/index.tsx
+++ src/pages/TranslationsPage/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TranslationsPage: React.FC = () => {
-  return <div>{/* Add content for the Translations page */}</div>;
+  return <div></div>;
 };
 
 export default TranslationsPage;
