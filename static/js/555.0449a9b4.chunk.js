9dd7d465a9e9b23d284e3e5e402c6e4d7c86fab9
Vlasvin <103183090+Vlasvin@users.noreply.github.com>
2024-05-25 17:13:12 +0000

    Deploying to gh-pages from @ Vlasvin/extras@747e7cb6f706aaaf3881ab2b56e0017ff8631a90 🚀

--- static/js/555.0449a9b4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[555],{1555:(p,s,a)=>{a.r(s),a.d(s,{default:()=>u});a(5043);var e=a(579);const u=()=>(0,e.jsx)("div",{})}}]);
-//# sourceMappingURL=555.0449a9b4.chunk.js.map
