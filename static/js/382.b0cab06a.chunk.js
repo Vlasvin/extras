c8dff122190ac0478a7ea4428e0d369a08e90f70
Vlasvin <103183090+Vlasvin@users.noreply.github.com>
2024-06-07 15:16:07 +0000

    Deploying to gh-pages from @ Vlasvin/extras@68815f2a9430c50fdc8c9261a379284f851edcee 🚀

---
+++ static/js/382.b0cab06a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[382],{9382:(a,s,e)=>{e.r(s),e.d(s,{default:()=>n});var t=e(8987),c=e(317),i=e(579);const n=()=>(0,i.jsx)(t.A,{titleKey:"visa_canada.title",images:c.rd,stepsKey:"visa_canada.steps",costsKey:"visa_canada.costs",costTitleKey:"visa_canada.cost_title",documentsKey:"visa_canada.documents",applyButtonKey:"visa_canada.apply_button",descriptionKey:"visa_canada.description",pageName:"VisaCanada"})}}]);
+//# sourceMappingURL=382.b0cab06a.chunk.js.map
